a8b2e105284f976018636cfc0252f1aee9e58349
Khan <loprommy55@gmail.com>
2024-11-08 11:23:39 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
   };
 
   const clickwithdraw = (amount) => {
-    setWithdrawAmount(amount);
+    setWithdrawAmount((prevAmount) => (prevAmount ? (parseInt(prevAmount) + amount).toString() : amount.toString()));
   };
 
   return (
